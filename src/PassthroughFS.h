79f2af937a13f212836b6d774776623e17cac5dc
lzoerner <leon_andre.zoerner@stud.htwk-leipzig.de>
2025-05-15 21:19:01 +0200

    style angepasst

--- src/PassthroughFS.h
+++ src/PassthroughFS.h
@@ -2,11 +2,12 @@
 #include <string>
 
 class PassthroughFS {
+private:
+    std::string backend;
+
 public:
     PassthroughFS(const std::string& backendPath);
     void mount(const std::string& mountPoint);
     void unmount();
-
-private:
-    std::string backend;
+    
 };
