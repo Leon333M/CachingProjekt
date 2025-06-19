6e1d0e69a676a8100279a665290bbcab540d7b7a
lzoerner <leon_andre.zoerner@stud.htwk-leipzig.de>
2025-06-19 13:28:47 +0200

    ShutdownEventFilter

--- src/GUI/GuiManager.cpp
+++ src/GUI/GuiManager.cpp
@@ -1,6 +1,7 @@
 // GuiManager.cpp
 #include "GuiManager.h"
 #include "MainWindow.h"
+#include "ShutdownEventFilter.h"
 
 GuiManager::GuiManager(int argc, char *argv[], ConfigLoader *controller)
     : configLoader(controller) {
@@ -15,6 +16,8 @@ GuiManager::~GuiManager() {
 
 void GuiManager::startGui(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    ShutdownEventFilter *filter = new ShutdownEventFilter();
+    app.installNativeEventFilter(filter);
     MainWindow mw = MainWindow(configLoader);
     app.exec();
 }
