263343122ee050b792cc8e76c65e11d3f8d96602
lzoerner <leon_andre.zoerner@stud.htwk-leipzig.de>
2025-06-19 16:08:38 +0200

    Benachrichtiger verbessert

--- src/Benachrichtiger.cpp
+++ src/Benachrichtiger.cpp
@@ -2,14 +2,40 @@
 #include "Benachrichtiger.h"
 #include "AufgabenVerteiler.h"
 #include <mutex>
+#include <shared_mutex>
 
 static AufgabenVerteiler dispatcher;
+std::shared_mutex sperre;
 
-void Benachrichtiger::registriereListener(std::function<void()> callback) {
+int Benachrichtiger::registriereListener(std::function<void()> callback) {
+    std::unique_lock<std::shared_mutex> lock(sperre);
     listener.push_back(std::move(callback));
+    int id = naechsteId++;
+    idZuIndex[id] = listener.size() - 1;
+    return id;
+}
+
+void Benachrichtiger::entferneListener(int id) {
+    std::unique_lock<std::shared_mutex> lock(sperre);
+    auto it = idZuIndex.find(id);
+    if (it != idZuIndex.end()) {
+        size_t index = it->second;
+        if (index < listener.size()) {
+            listener.erase(listener.begin() + index);
+            idZuIndex.erase(it);
+
+            // Alle nachfolgenden Indizes aktualisieren
+            for (auto &[key, val] : idZuIndex) {
+                if (val > index) {
+                    val--;
+                }
+            }
+        }
+    }
 }
 
 void Benachrichtiger::benachrichtigeListenerAsync() {
+    std::shared_lock<std::shared_mutex> lock(sperre);
     for (const auto &f : listener) {
         dispatcher.einreihen(f); // Fuhrt Listener asynchron aus
     }
--- src/Benachrichtiger.h
+++ src/Benachrichtiger.h
@@ -6,13 +6,16 @@
 class Benachrichtiger {
 private:
     std::vector<std::function<void()>> listener;
+    std::unordered_map<int, int> idZuIndex; // ID → Index
+    int naechsteId = 0;
     // AufgabenVerteiler dispatcher; // in cpp
 
 public:
     virtual ~Benachrichtiger() = default;
 
     // Hier konnen sich andere Klassen registrieren
-    void registriereListener(std::function<void()> callback);
+    int registriereListener(std::function<void()> callback);
+    void entferneListener(int id);
 
 protected:
     // Diese Methode rufst du im abgeleiteten Objekt auf, wenn sich etwas andert
