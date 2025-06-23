5c07ae3528ace1fdd4c49b70e53d548ff4b68bfd
lzoerner <leon_andre.zoerner@stud.htwk-leipzig.de>
2025-06-23 19:52:56 +0200

    CacheEditor layout

--- src/GUI/CacheEditor.cpp
+++ src/GUI/CacheEditor.cpp
@@ -5,11 +5,28 @@
 CacheEditor::CacheEditor(CacheInterface *cacheInterface)
     : cache(cacheInterface),
       windowLayout(QGridLayout(this)) {
-    windowLayout.addWidget(new QLabel(QString::fromWCharArray(cacheInterface->getCacheName().c_str())));
-    windowLayout.addWidget(&labelCacheTyp);
-    windowLayout.addWidget(&labelCurrentCacheSize);
-    windowLayout.addWidget(&labelMaxCacheSize);
-    windowLayout.addWidget(&labelMinZugriffsHaufigkeit);
+    QGridLayout *layout1 = new QGridLayout();
+    layout1->addWidget(new QLabel("CacheName:"), 0, 0);
+    layout1->addWidget(new QLabel(QString::fromWCharArray(cacheInterface->getCacheName().c_str())), 0, 1);
+    windowLayout.addLayout(layout1, 0, 0);
+    QGridLayout *layout2 = new QGridLayout();
+    layout2->addWidget(new QLabel("CacheTyp:"), 0, 0);
+    layout2->addWidget(&labelCacheTyp, 0, 1);
+    windowLayout.addLayout(layout2, 1, 0);
+    if (cache->getCacheTyp() != L"Cache") {
+        QGridLayout *layout3 = new QGridLayout();
+        layout3->addWidget(new QLabel("CurrentCacheSize:"), 0, 0);
+        layout3->addWidget(&labelCurrentCacheSize, 0, 1);
+        windowLayout.addLayout(layout3, 2, 0);
+        QGridLayout *layout4 = new QGridLayout();
+        layout4->addWidget(new QLabel("MaxCacheSize:"), 0, 0);
+        layout4->addWidget(&labelMaxCacheSize, 0, 1);
+        windowLayout.addLayout(layout4, 3, 0);
+        QGridLayout *layout5 = new QGridLayout();
+        layout5->addWidget(new QLabel("MinZugriffsHaufigkeit:"), 0, 0);
+        layout5->addWidget(&labelMinZugriffsHaufigkeit, 0, 1);
+        windowLayout.addLayout(layout5, 4, 0);
+    }
     refresh();
 }
 
@@ -20,8 +37,8 @@ CacheEditor::~CacheEditor() {
 }
 
 void CacheEditor::refresh() {
+    labelCacheTyp.setText(QString::fromWCharArray(cache->getCacheTyp().c_str()));
     if (cache->getCacheTyp() != L"Cache") {
-        labelCacheTyp.setText(QString::fromWCharArray(cache->getCacheTyp().c_str()));
         labelCurrentCacheSize.setText(QString::number(cache->getCurrentCacheSize()));
         labelMaxCacheSize.setText(QString::number(cache->getMaxCacheSize()));
         labelMinZugriffsHaufigkeit.setText(QString::number(cache->getMinZugriffsHaufigkeit()));
